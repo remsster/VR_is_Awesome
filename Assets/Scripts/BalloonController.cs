602dfd0e0cd2e47c7ec1a964d91dffe597854fb5
Remi Tobias <remitobias1@outlook.com>
2022-03-02 20:07:43 +0000

    Balloon World
    A scene where the player can create balloons from their hands.

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+
+    public GameObject balloonPrefab;
+    public float floatStrengh = 20f;
+    public float growthRate = 1.5f;
+    private GameObject balloon;
+    //private ButtonInputController buttonController;
+
+    private void Start()
+    {
+        //buttonController.ButtonDownEvent.AddListener(CreateBalloon);
+        //buttonController.ButtonUpEvent.AddListener(ReleaseBalloon);
+    }
+
+    void Update()
+    {
+        //if (Input.GetButtonDown("XRI_Right_TriggerButton")) { CreateBalloon(); }
+        //else if (Input.GetButtonUp("XRI_Right_TriggerButton")) { ReleaseBalloon(); }
+        if (balloon != null) { GrowBalloon(); }
+    }
+
+    private void GrowBalloon()
+    {
+        float growThisFrame = growthRate * Time.deltaTime;
+        Vector3 changeScale = balloon.transform.localScale * growThisFrame;
+        balloon.transform.localScale += changeScale;
+    }
+
+    public void ReleaseBalloon()
+    {
+        balloon.transform.parent = null;
+        balloon.GetComponent<Rigidbody>().AddForce(Vector3.up * floatStrengh);
+        GameObject.Destroy(balloon, 10f);
+        balloon = null;
+    }
+
+    public void CreateBalloon(GameObject parentHand)
+    {
+        balloon = Instantiate(balloonPrefab, parentHand.transform);
+        balloon.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
+}
---
+++ Assets/Scripts/ButtonInputController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInputController : MonoBehaviour
+{
+    public UnityEvent ButtonDownEvent = new UnityEvent();
+    public UnityEvent ButtonUpEvent = new UnityEvent();
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("XRI_Right_TriggerButton")) ButtonDownEvent.Invoke();
+        else if (Input.GetButtonUp("XRI_Right_TriggerButton")) ButtonUpEvent.Invoke();
+    }
+}
